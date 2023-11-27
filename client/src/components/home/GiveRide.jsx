aefa1b05d5128617c8acc92750e85bf594d91867
Nirbhay Singh <nirbhaymahan100@gmail.com>
2023-11-27 22:59:35 +0530

    google maps api integrated

--- client/src/components/home/GiveRide.jsx
+++ client/src/components/home/GiveRide.jsx
@@ -1,35 +1,136 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
-import "./rides.css"
-import recentericon from "../../images/center.png"
+import "./rides.css";
+import recentericon from "../../images/center.png";
 import searchicon from "../../images/loupe.png";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { useRef, useState, useEffect } from "react";
 
-export default function GiveRides(){
-    return(
-        <div className="give">
-            
-            <div className="map-container">
-                
-            </div>
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
 
+function GiveRides() {
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
 
-            <div className="location-inputs">
-                <div className="destination-input source">
-                    <input className="input-box" required></input>
-                </div>
-                <div className="destination-input desti">
-                    <input className="input-box" required></input>
-                    <img src={searchicon} alt=""></img>
-                </div>
-            </div>
-            
+  useEffect(() => {
+    const position = () => {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setLat(pos.coords.latitude);
+        setLon(pos.coords.longitude);
+      });
+    };
 
+    position();
+  }, []);
 
+  const center = { lat: lat, lng: lon };
 
-            <div className="recenter">
-                <img src={recentericon} alt=""></img>
-            </div>
-            <Navbar></Navbar>
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAVhzeezfxXEycEp3RRClOSpjQxdQ8fGzo",
+    libraries: ["places"],
+  });
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef();
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destiantionRef = useRef();
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+  }
+
+  //   function clearRoute() {
+  //     setDirectionsResponse(null)
+  //     originRef.current.value = ''
+  //     destiantionRef.current.value = ''
+  //   }
+  return isLoaded ? (
+    <div className="give">
+      <div className="map-container">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={15}
+          // onLoad={onLoad}
+          // onUnmount={onUnmount}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </div>
+
+      {/* <div className="location-inputs">
+        <div className="destination-input source">
+          <Autocomplete>
+            <input
+              className="input-box"
+              required
+              type="text"
+              placeholder="Source"
+              ref={originRef}
+            ></input>
+          </Autocomplete>
         </div>
-    )
+        <div className="destination-input desti">
+          <Autocomplete>
+            <input
+              className="input-box"
+              required
+              type="text"
+              placeholder="Destination"
+              ref={destiantionRef}
+            ></input>
+          </Autocomplete>
+          <img src={searchicon} alt="" onClick={calculateRoute}></img>
+        </div>
+      </div> */}
+
+      <div
+        className="recenter"
+        onClick={() => {
+          map.panTo(center);
+        }}
+      >
+        <img src={recentericon} alt=""></img>
+      </div>
+      <Navbar></Navbar>
+    </div>
+  ) : (
+    <></>
+  );
 }
+
+export default React.memo(GiveRides);
